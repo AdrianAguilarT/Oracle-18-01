a5329bee5231a085b270990f74e75420d0ad0603
AdrianAguilarT <31869875+AdrianAguilarT@users.noreply.github.com>
2017-10-07 10:46:30 -0500

    Add files via upload

--- Curso-BD2.sql
+++ Curso-BD2.sql
@@ -1,7 +1,24 @@
 
 -- Clase de Sábado 7 de Octubre
-CREATE TABLE Almacén(
-  numero_almacen integer,
-  ubicacion_almacen varchar2(80),
+CREATE TABLE almacen(
+  numero_almacen INTEGER,
+  ubicacion_almacen VARCHAR2(80),
   CONSTRAINT pk_na PRIMARY KEY(numero_almacen)
 );
+
+-- Generación de procedimiento almacenado para guardar una entidad o registro de tipo almacén
+CREATE OR REPLACE PROCEDURE guardar_almacen(mi_id IN INTEGER, mi_ubicacion IN VARCHAR2)
+  AS
+  BEGIN
+  -- Lógica del procedimiento
+  INSERT INTO almacen VALUES(mi_id, mi_ubicacion);
+END;
+/
+
+-- Prueba de Procedimiento
+BEGIN
+  guardar_almacen(1, 'Plymouth');
+END;
+/
+
+SELECT * FROM almacen;
